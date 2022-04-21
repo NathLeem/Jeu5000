b086004df50df4528d2c35c5701263ee436050e6
NathLeem <91312993+NathLeem@users.noreply.github.com>
2022-04-21 15:54:39 +0200

    Add files via upload

---
+++ Jeu5000/Jeu5000/MethodeDuProjet.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeu5000
+{
+    public struct MethodeDuProjet
+    {
+        public void ChoixNbreJoueur(int NbreJoueur, ref string player1, ref string player2, ref string player3, ref string player4, ref string player5)
+        {
+            if (NbreJoueur >= 2)
+            {
+                Console.WriteLine("Veuillez entrer le prénom du premier joueur");
+                player1 = Console.ReadLine();
+                Console.WriteLine("Veuillez entrer le prénom du deuxième joueur");
+                player2 = Console.ReadLine();
+            }
+            if (NbreJoueur >= 3)
+            {
+                Console.WriteLine("Veuillez entrer le prénom du troisième joueur");
+                player3 = Console.ReadLine();
+            }
+            if (NbreJoueur >= 4)
+            {
+                Console.WriteLine("Veuillez entrer le prénom du quatrième joueur");
+                player4 = Console.ReadLine();
+            }
+            if (NbreJoueur >= 5)
+            {
+                Console.WriteLine("Veuillez entrer le prénom du cinquième joueur");
+                player5 = Console.ReadLine();
+            }
+
+            Console.ReadLine();
+        }
+        public void lancerDes(int[,] tablDes, int NbreJoueur)
+        {
+            NbreJoueur = 0;//na
+
+        }
+        public void AfficheTableau(int[,] tablDes, int NbreJoueur)
+        {
+            for (int i = 0; i < NbreJoueur; i++)
+            {
+                for (int j = 0; j < NbreJoueur; j++)
+                {
+                    Console.Write(tablDes[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Jeu5000/Jeu5000/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeu5000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string player1 = "";
+            string player2 = "";
+            string player3 = "";
+            string player4 = "";
+            string player5 = "";
+            int NbreJoueur = 0;
+            
+
+            Console.WriteLine("-------------");
+            Console.Write("|");
+            Console.Write("Jeu du 5000");
+            Console.WriteLine("|");
+            Console.WriteLine("-------------");
+            Console.WriteLine("Règles du jeu");
+            Console.WriteLine("////");
+
+            Console.WriteLine("Combien de joueurs vont jouer ?");
+            NbreJoueur = int.Parse(Console.ReadLine());
+
+            int[,] tablDes = new int[NbreJoueur, 5];
+            MethodeDuProjet MesOutils = new MethodeDuProjet();
+
+            MesOutils.AfficheTableau(tablDes, NbreJoueur);
+            MesOutils.ChoixNbreJoueur(NbreJoueur, ref player1, ref player2, ref player3, ref player4, ref player5);
+
+            Console.WriteLine($"1er Joueur: {player1}");
+            Console.WriteLine($"2eme joueur:{player2}");
+            Console.WriteLine($"3eme joueur: {player3}");
+            Console.WriteLine($"4eme joueur: {player4}");
+            Console.WriteLine($"5eme joueur: {player5}");
+
+            Console.ReadLine();
+        }
+    }
+}
